ca94623423c7fa8d46894325111de0986470afae
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2024-10-16 11:10:49 +0800

    Update xary.sh

--- xary.sh
+++ xary.sh
@@ -4,7 +4,7 @@ printf "\e[92m"
 printf "                       |\\__/,|   (\\\\ \n"
 printf "                     _.|o o  |_   ) )\n"
 printf "       -------------(((---(((-------------------\n"
-printf "                       catmi \n"
+printf "                     catmi.xary \n"
 printf "       -----------------------------------------\n"
 printf "\e[0m"
 DEFAULT_START_PORT=20000                         # 默认起始端口
