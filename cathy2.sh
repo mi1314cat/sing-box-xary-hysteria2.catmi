224b8c019193da8d898d5f864f1c2e68bf9d07c9
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2024-10-19 00:13:22 +0800

    Update cathy2.sh

--- cathy2.sh
+++ cathy2.sh
@@ -123,12 +123,8 @@ modify_config() {
         return 1
     fi
 
-    # 调试信息，检查当前密码
-    echo "当前服务端密码: ${current_password}"
-    echo "新的服务端密码: ${new_password}"
-
-    # 修改服务端密码
-    if sed -i "s|^  password: .*|  password: ${new_password}|" /etc/hysteria/config.yaml; then
+    # 尝试更宽松地匹配密码行
+    if sed -i "s|^ *password: .*|  password: ${new_password}|" /etc/hysteria/config.yaml; then
         echo "成功修改服务端的密码"
     else
         echo "修改服务端的密码失败"
@@ -172,6 +168,9 @@ modify_config() {
         echo "重启服务失败"
     fi
 }
+
+
+
 # 主菜单
 show_menu() {
     # 获取服务状态
