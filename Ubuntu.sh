bd22dd4d86b01920efbf37a2f0ce865bf3af229d
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2024-10-20 11:48:21 +0800

    Update Ubuntu.sh

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -8,11 +8,14 @@ printf "                    catmi-一键脚本 \n"
 printf "       -----------------------------------------\n"
 printf "\e[0m"
 apt-get update -y && apt install curl -y && apt install sudo -y && apt install nano -y
+
 # 添加回车等待
 read -p "按回车继续执行第一个脚本..."
+clear
 bash <(curl -fsSL https://github.com/mi1314cat/sing-box-xary-hysteria2.catmi/raw/refs/heads/main/cathy2.sh)
 # 添加回车等待
 read -p "按回车继续执行第二个脚本..."
+clear
 bash <(curl -fsSL https://github.com/mi1314cat/sing-box-xary-hysteria2.catmi/raw/main/install.sh)
 # 添加回车等待
 read -p "按回车继续执行第三个脚本..."
