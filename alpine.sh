26ce1d1a9e4255c6dd17a2b287cc91dbd9221102
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2024-10-20 11:28:00 +0800

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -9,5 +9,9 @@ printf "       -----------------------------------------\n"
 printf "\e[0m"
 apk add update
 apk add wget bash curl sudo
+
 bash <(curl -fsSL https://github.com/mi1314cat/sing-box-xary-hysteria2.catmi/raw/refs/heads/main/alpine-hysteria2.sh)
+# 添加回车等待
+read -p "按回车继续执行第二个脚本..."
+
 curl -sS -O https://raw.githubusercontent.com/kejilion/sh/main/kejilion.sh && chmod +x kejilion.sh && ./kejilion.sh
