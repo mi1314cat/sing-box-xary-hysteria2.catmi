5bd2815775fb8a7eefec5e9be836d8458a94c9b9
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2024-10-20 11:47:21 +0800

    Update alpine.sh

--- alpine.sh
+++ alpine.sh
@@ -9,6 +9,7 @@ printf "       -----------------------------------------\n"
 printf "\e[0m"
 apk add update
 apk add wget bash curl sudo
+clear
 
 bash <(curl -fsSL https://github.com/mi1314cat/sing-box-xary-hysteria2.catmi/raw/refs/heads/main/alpine-hysteria2.sh)
 # 添加回车等待
