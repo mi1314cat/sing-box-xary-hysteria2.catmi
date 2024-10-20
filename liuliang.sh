5268f31475bf4dfa31682aeb24c7797b772ef7c2
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2024-10-20 13:59:24 +0800

    Update liuliang.sh

--- liuliang.sh
+++ liuliang.sh
@@ -51,7 +51,7 @@ write_config() {
 
 # Get traffic statistics
 get_traffic() {
-  interface=$(ls /sys/class/net | grep -v lo | head -n 1)
+  interface=$(ls /sys/class/net | grep -v 'lo' | head -n 1)
   if [ -z "$interface" ]; then
     echo "Failed to get network interface"
     exit 1
@@ -79,7 +79,7 @@ output_status() {
       if (tx_total > 1024) { tx_total /= 1024; tx_units = "MB"; }
       if (tx_total > 1024) { tx_total /= 1024; tx_units = "GB"; }
 
-      printf("总接收: %.2f %s\n总发送: %.2f %s\n", rx_total, rx_units, tx_total, tx_units);
+      print("总接收: %.2f %s\n总发送: %.2f %s\n", rx_total, rx_units, tx_total, tx_units);
     }' /proc/net/dev)
   echo "$output"
 }
@@ -103,9 +103,9 @@ execute_command() {
 main_menu() {
   while true; do
     clear
-    echo "流量监控和指令执行功能"
+    echo "流量监控和命令执行功能"
     echo "------------------------------------------------"
-    echo "当前流量使用情况，重启服务器流量计算会清零！"
+    echo "当前流量统计"
     get_traffic
     output_status
     echo "当前进站流量: $(cat ~/current_rx_gb.txt) GB"
@@ -115,25 +115,24 @@ main_menu() {
     echo "当前设置的进站限流阈值为: $rx_threshold_gb GB"
     echo "当前设置的出站限流阈值为: $tx_threshold_gb GB"
     echo "当前流量重置日期: $cz_day"
-    echo "当前执行指令: $command"
+    echo "当前执行命令: $command"
     echo "------------------------------------------------"
-    echo "系统每分钟会检测实际流量是否达到阈值，达到后会自动执行指令！"
-    read -e -p "1. 设置限流阈值    2. 设置执行指令    3. 启用流量监控    4. 停用流量监控    0. 退出  : " choice
+    read -e -p "选择操作: 1. 设置限流阈值 2. 设置执行命令 3. 启用流量监控 4. 停用流量监控 0. 退出: " choice
 
     case "$choice" in
       1)
-        read -e -p "请输入进站流量阈值（单位为GB）: " rx_threshold_gb
-        read -e -p "请输入出站流量阈值（单位为GB）: " tx_threshold_gb
-        read -e -p "请输入流量重置日期（默认每月1日重置）: " cz_day
+        read -e -p "输入进站流量阈值（单位为GB）: " rx_threshold_gb
+        read -e -p "输入出站流量阈值（单位为GB）: " tx_threshold_gb
+        read -e -p "输入流量重置日期（默认每天重置）: " cz_day
         cz_day=${cz_day:-1}
         write_config
         setup_cron
         echo "限流阈值已设置"
         ;;
       2)
-        read -e -p "请输入要执行的指令: " command
+        read -e -p "输入要执行的命令: " command
         write_config
-        echo "执行指令已设置"
+        echo "执行命令已设置"
         ;;
       3)
         setup_cron
@@ -156,15 +155,15 @@ main_menu() {
 # Setup cron job
 setup_cron() {
   (crontab -l ; echo "* * * * * $HOME/liuliang.sh check") | crontab -
-  (crontab -l ; echo "1 1 $cz_day * * reboot") | crontab -
-  echo "定时任务已设置"
+  (crontab -l ; echo "0 0 $cz_day * * $command") | crontab -
+  echo "已设置定时任务"
 }
 
 # Remove cron job
 remove_cron() {
   crontab -l | grep -v "$HOME/liuliang.sh check" | crontab -
-  crontab -l | grep -v "reboot" | crontab -
-  echo "定时任务已移除"
+  crontab -l | grep -v "$command" | crontab -
+  echo "已删除定时任务"
 }
 
 main_menu
